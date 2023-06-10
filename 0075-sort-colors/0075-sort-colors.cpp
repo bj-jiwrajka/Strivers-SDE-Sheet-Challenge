8e0670ac1402255704f3f21f767464022379aaa6
Bijay Jiwrajka <jiwrajkabijay@gmail.com>
2023-06-10 15:08:36 +0530

    Time: 3 ms (48.23%), Space: 8.2 MB (49.12%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,25 +1,40 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int a=0,b=0,c=0;
         int n=nums.size();
-        for(int i=0;i<n;i++)
+        int i=0,j=n-1;
+        for(int k=i;k<=j;k++)
         {
-            if(!nums[i]) a++;
-            else if(nums[i]==1) b++;
-            else c++;
-        }
-        for(int i=0;i<a;i++)
-        {
-            nums[i]=0;
-        }
-        for(int i=a;i<a+b;i++)
-        {
-            nums[i]=1;
-        }
-        for(int i=a+b;i<a+b+c;i++)
-        {
-            nums[i]=2;
+            if(nums[k]==0)
+                swap(nums[k],nums[i++]);
+            else if(nums[k]==2)
+                swap(nums[k--],nums[j--]);
         }
     }
 };
+
+
+
+
+//naive approach
+
+        // int a=0,b=0,c=0;
+        // int n=nums.size();
+        // for(int i=0;i<n;i++)
+        // {
+        //     if(!nums[i]) a++;
+        //     else if(nums[i]==1) b++;
+        //     else c++;
+        // }
+        // for(int i=0;i<a;i++)
+        // {
+        //     nums[i]=0;
+        // }
+        // for(int i=a;i<a+b;i++)
+        // {
+        //     nums[i]=1;
+        // }
+        // for(int i=a+b;i<a+b+c;i++)
+        // {
+        //     nums[i]=2;
+        // }
