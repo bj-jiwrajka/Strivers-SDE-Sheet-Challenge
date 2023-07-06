c39ec9e984996646bcb2898e5b47dee97001b6a6
Bijay Jiwrajka <jiwrajkabijay@gmail.com>
2023-07-06 12:34:48 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (38.31%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,27 @@
+class Solution
+{
+    public:
+        bool isValid(string s)
+        {
+            stack<char> st;
+            for (auto it: s)
+            {
+                if (it == '('
+                    or it == '{'
+                    or it == '[')
+                    st.push(it);
+                else if (st.empty() or (st.top() == '{'
+                        and it != '}') or(st.top() == '['
+                        and it != ']') or(st.top() == '('
+                        and it != ')'))
+                {
+                    return false;
+                }
+                else
+                {
+                    st.pop();
+                }
+            }
+            return st.empty();
+        }
+};
